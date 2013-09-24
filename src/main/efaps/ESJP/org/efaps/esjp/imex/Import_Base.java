78d2d3ae8ccbf7ba5224b0b8e92196f85a6a0194
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-09-24 21:23:03 +0000

    - importexport: adding sequence
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/importexport/trunk@10301 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/imex/Import_Base.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2003 - 2013 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+
+package org.efaps.esjp.imex;
+
+import java.util.UUID;
+
+import org.efaps.admin.common.NumberGenerator;
+import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Return;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.db.Insert;
+import org.efaps.esjp.ci.CIImEx;
+import org.efaps.esjp.common.uiform.Create;
+import org.efaps.util.EFapsException;
+
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+@EFapsUUID("8043bb61-7008-425f-b1f6-8300ac580e3c")
+@EFapsRevision("$Rev$")
+public abstract class Import_Base
+{
+
+    public Return create(final Parameter _parameter)
+        throws EFapsException
+    {
+        final Create create = new Create()
+        {
+
+            @Override
+            protected void add2basicInsert(final Parameter _parameter,
+                                           final Insert _insert)
+                throws EFapsException
+            {
+                // ImEx_ImportSequence
+                final String name = NumberGenerator.get(UUID.fromString("2f0e1989-3170-4226-a1c3-7aae7e532ff1"))
+                                .getNextVal();
+                _insert.add(CIImEx.Import.Name, name);
+            }
+        };
+        return create.execute(_parameter);
+    }
+}
